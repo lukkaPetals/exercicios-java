033f1fdf51edc91c94bc75aef55f64a5107e4bc0
lukkaPetals <107210315+lukkaPetals@users.noreply.github.com>
2022-08-27 18:47:12 -0300

    primeira commit

---
+++ ex2.java
@@ -0,0 +1,14 @@
+public class ex2 {
+    public static void main(String[] args) {
+
+    int pesoHumano, limiteDePeso, numeroDePessoas;
+    //você pode tambem usar o comando "var" para declarar uma variavel sem tipagem
+
+    pesoHumano = 150;
+    limiteDePeso = 1400;
+    numeroDePessoas = limiteDePeso / pesoHumano;
+
+    System.out.print("Você pode acomodar " + numeroDePessoas + " pessoas no elevador");
+        
+    }
+}
